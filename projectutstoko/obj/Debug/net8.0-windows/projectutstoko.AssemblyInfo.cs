4307ab6562fe17a5ed15e5118a8368cf940e03f5
Agus Miftachul Huda <agusmiftachulhuda44@gmail.com>
2025-09-02 15:06:47 +0700

    Up

--- projectutstoko/obj/Debug/net8.0-windows/projectutstoko.AssemblyInfo.cs
+++ projectutstoko/obj/Debug/net8.0-windows/projectutstoko.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projectutstoko")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6906a781d02888d0a1e85433271220f6d14448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68201ec1fff9d295945e49909034b5e8826509a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("projectutstoko")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projectutstoko")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
